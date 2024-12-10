18eb3ffbba68ba660054a966d910f5b9a34ae395
JonF12 <jonas11ur@gmail.com>
2024-12-10 23:06:45 +0000

    more nvim changes add dap remove old stuff

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -35,5 +35,12 @@ map("n", "<C-Down>", "<C-w>j", { desc = "Window down" })
 map("n", "<C-Up>", "<C-w>k", { desc = "Window up" })
 map("n", "<C-Right>", "<C-w>l", { desc = "Window right" })
 map("n", "<leader>X", close_all_but_this, { desc = "Close All all bufs but this" })
-
 map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+
+--debugger
+-- map("n", "<leader>bb", "<cmd>DapToggleBreakpoint<CR>", { desc = "Toggle Breakpoint" })
+-- map("n", "<F5>", "<cmd>DapContinue<CR>", { desc = "Start/Continue" })
+-- map("n", "<F10>", "<cmd>DapStepOver<CR>", { desc = "Step Over" })
+-- map("n", "<F11>", "<cmd>DapStepInto<CR>", { desc = "Step Into" })
+-- map("n", "<F6>", "<cmd>DapStepOut<CR>", { desc = "Step Out" })
+-- map("n", "<F2>", "<cmd>DapTerminate<CR>", { desc = "Terminate" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,56 @@
+local config_dap = function()
+  local dap = require "dap"
+  local ui = require "dapui"
+
+  require("dapui").setup()
+  require("dap-go").setup()
+
+  vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+  vim.keymap.set("n", "<F5>", dap.continue, { desc = "Start/Continue" })
+  vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step Over" })
+  vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step Into" })
+  vim.keymap.set("n", "<F9>", dap.step_out, { desc = "Step Out" })
+
+  -- Add highlight groups for DAP signs
+  vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#ff0000" }) -- Red
+  vim.api.nvim_set_hl(0, "DapBreakpointCondition", { fg = "#0000ff" }) -- Blue
+  vim.api.nvim_set_hl(0, "DapLogPoint", { fg = "#00ff00" }) -- Green
+  vim.api.nvim_set_hl(0, "DapStopped", { fg = "#ffff00" }) -- Yellow
+  vim.api.nvim_set_hl(0, "DapBreakpointRejected", { fg = "#ff6b6b" }) -- Light red
+
+  -- Then your existing sign definitions
+  vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+  vim.fn.sign_define(
+    "DapBreakpointCondition",
+    { text = "🔵", texthl = "DapBreakpointCondition", linehl = "", numhl = "" }
+  )
+  vim.fn.sign_define("DapLogPoint", { text = "📝", texthl = "DapLogPoint", linehl = "", numhl = "" })
+  vim.fn.sign_define(
+    "DapStopped",
+    { text = "👉", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+  )
+  vim.fn.sign_define(
+    "DapBreakpointRejected",
+    { text = "⭕", texthl = "DapBreakpointRejected", linehl = "", numhl = "" }
+  )
+
+  dap.listeners.before.attach.dapui_config = function()
+    ui.open()
+  end
+
+  dap.listeners.before.launch.dapui_config = function()
+    ui.open()
+  end
+
+  dap.listeners.before.event_terminated.dapui_config = function()
+    ui.close()
+  end
+
+  dap.listeners.before.event_exited.dapui_config = function()
+    ui.close()
+  end
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -11,6 +64,36 @@ return {
     end,
   },
   {
+    "mfussenegger/nvim-dap",
+    lazy = false,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "leoluz/nvim-dap-go",
+    },
+    config = config_dap,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    lazy = false,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("dapui").setup()
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    lazy = false,
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      require("dap-go").setup()
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
@@ -19,6 +102,7 @@ return {
         "yaml-language-server",
         "omnisharp",
         "denopls",
+        "delve",
         "templ",
         "prettier",
         "csharpier",
