9ee2b5b5b253e346791ae7d1d86f3657b1a6ec82
JonF12 <jonas11ur@gmail.com>
2025-02-02 18:39:59 +0000

    .

--- lua/custom/autocmd.lua
+++ lua/custom/autocmd.lua
@@ -1,21 +1,21 @@
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    require("nvim-tree.api").tree.open()
-  end,
+	callback = function()
+		require("nvim-tree.api").tree.open()
+	end,
 })
 
 -- Add this event handler
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("UFO_GROUP", { clear = true }),
-  callback = function()
-    vim.cmd("silent! loadview")
-  end,
+	group = vim.api.nvim_create_augroup("UFO_GROUP", { clear = true }),
+	callback = function()
+		vim.cmd("silent! loadview")
+	end,
 })
 
 -- C/C++ use line comments instead of block comments
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
-  callback = function()
-    vim.bo.commentstring = "// %s"
-  end,
+	pattern = { "c", "cpp" },
+	callback = function()
+		vim.bo.commentstring = "// %s"
+	end,
 })
