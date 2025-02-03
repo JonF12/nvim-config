6ac6847cdd1591424d80d02649b2237aebfb5751
JonF12 <jonas11ur@gmail.com>
2025-02-03 23:02:17 +0000

    .

--- lua/configs/languages/webdev.lua
+++ lua/configs/languages/webdev.lua
@@ -2,69 +2,69 @@ local lspconfig = require("lspconfig")
 local nvlsp = require("nvchad.configs.lspconfig")
 
 lspconfig.cssls.setup({
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
 })
 
 lspconfig.tailwindcss.setup({
-  on_attach = nvlsp.on_attach,
-  capabilities = nvlsp.capabilities,
-  filetypes = {
-    "templ",
-    "javascript",
-    "typescript",
-    "react",
-    "html",
-    "css",
-    "scss",
-    "javascriptreact",
-    "typescriptreact",
-    "vue",
-    "svelte",
-  },
-  init_options = {
-    userLanguages = {
-      templ = "html",
-    },
-  },
-  root_dir = lspconfig.util.root_pattern("tailwind.config.js", "postcss.config.js"),
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	filetypes = {
+		"templ",
+		"javascript",
+		"typescript",
+		"react",
+		"html",
+		"css",
+		"scss",
+		"javascriptreact",
+		"typescriptreact",
+		"vue",
+		"svelte",
+	},
+	init_options = {
+		userLanguages = {
+			templ = "html",
+		},
+	},
+	root_dir = lspconfig.util.root_pattern("tailwind.config.js", "postcss.config.js"),
 })
 
 lspconfig.html.setup({
-  on_attach = nvlsp.on_attach,
-  capabilities = nvlsp.capabilities,
-  filetypes = { "html", "templ", "cshtml" },
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	filetypes = { "html", "templ", "cshtml" },
 })
 lspconfig.emmet_ls.setup({
-  capabilities = nvlsp.capabilities,
-  filetypes = { "html", "css", "templ" },
-  init_options = {
-    html = {
-      options = {
-        ["bem.enabled"] = true,
-      },
-    },
-  },
+	capabilities = nvlsp.capabilities,
+	filetypes = { "html", "css", "templ" },
+	init_options = {
+		html = {
+			options = {
+				["bem.enabled"] = true,
+			},
+		},
+	},
 })
 -- Custom server configurations
 -- TypeScript/JavaScript with additional settings
 lspconfig.ts_ls.setup({
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  root_dir = lspconfig.util.root_pattern("package.json"),
-  single_file_support = false,
-  capabilities = nvlsp.capabilities,
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
-    },
-  },
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	root_dir = lspconfig.util.root_pattern("package.json"),
+	single_file_support = false,
+	capabilities = nvlsp.capabilities,
+	init_options = {
+		preferences = {
+			disableSuggestions = true,
+		},
+	},
 })
 
 lspconfig.denols.setup({
-  on_attach = nvlsp.on_attach,
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+	on_attach = nvlsp.on_attach,
+	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 })
 
-lspconfig.htmx.setup({})
+-- lspconfig.htmx.setup({})
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -4,84 +4,99 @@ local lspkind = require("lspkind")
 local MAX_LABEL_WIDTH = 40
 local MIN_LABEL_WIDTH = 40
 local plugins = {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      completion = {
-        keyword_length = 2,
-        col_offset = -3,
-        side_padding = 0,
-      },
-      performance = {
-        max_view_entries = 50,
-      },
-      sorting = {
-        priority_weight = 2,
-        comparators = {
-          cmp.config.compare.exact,
-          cmp.config.compare.offset,
-          cmp.config.compare.score,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
-      },
-      formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          maxwidth = {
-            menu = 40,
-            abbr = 33,
-          },
-          show_labelDetails = false,
-          before = function(entry, vim_item)
-            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = (strings[1] or "")
-            kind.menu = (strings[2] or "")
+	{
+		"hrsh7th/nvim-cmp",
+		opts = {
+			completion = {
+				keyword_length = 2,
+				col_offset = -3,
+				side_padding = 0,
+			},
+			performance = {
+				max_view_entries = 50,
+			},
+			sorting = {
+				priority_weight = 2,
+				comparators = {
+					cmp.config.compare.exact,
+					cmp.config.compare.offset,
+					cmp.config.compare.score,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
+				},
+			},
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = {
+						menu = 40,
+						abbr = 33,
+					},
+					show_labelDetails = false,
+					before = function(entry, vim_item)
+						local kind =
+							require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = (strings[1] or "")
+						kind.menu = (strings[2] or "")
 
-            local label = kind.abbr
-            local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
-            if truncated_label ~= label then
-              kind.abbr = truncated_label
-            elseif string.len(label) < MIN_LABEL_WIDTH then
-              local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
-              kind.abbr = label .. padding
-            end
-            return kind
-          end,
-        }),
-      },
-      window = {
-        documentation = {
-          max_width = 45,
-          max_height = 45,
-        },
-      },
-    },
-  },
-  {
-    "d7omdev/nuget.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("nuget").setup()
-    end,
-  },
+						local label = kind.abbr
+						local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+						if truncated_label ~= label then
+							kind.abbr = truncated_label
+						elseif string.len(label) < MIN_LABEL_WIDTH then
+							local padding = string.rep(" ", MIN_LABEL_WIDTH - string.len(label))
+							kind.abbr = label .. padding
+						end
+						return kind
+					end,
+				}),
+			},
+			window = {
+				documentation = {
+					max_width = 45,
+					max_height = 45,
+				},
+			},
+		},
+	},
+	{
+		"d7omdev/nuget.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("nuget").setup()
+		end,
+	},
+	{
+		"ellisonleao/glow.nvim",
+		ft = "markdown",
+		config = function()
+			require("glow").setup({
+				width = 200,
+				height = 50,
+				width_ratio = 1,
+				height_ratio = 1,
+				style = "dark",
+				pager = false,
+			})
+		end,
+	},
 }
 
 require("ufo").setup({
-  provider_selector = function(bufnr, filetype, buftype)
-    if filetype == "yaml" then
-      return "indent" -- Single provider for yaml
-    end
-    return { "lsp", "indent" } -- Main and fallback for others
-  end,
+	provider_selector = function(bufnr, filetype, buftype)
+		if filetype == "yaml" then
+			return "indent" -- Single provider for yaml
+		end
+		return { "lsp", "indent" } -- Main and fallback for others
+	end,
 })
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
